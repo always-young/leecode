268b8ac6f4b28dc515119538b8d116f91f92e9d6
kevin <youaremyall.WEN>
2021-03-08 20:14:06 +0800

    chore:add the code

--- src/main/java/com/kevin/leecode/online/middle/LinkedList.java
+++ src/main/java/com/kevin/leecode/online/middle/LinkedList.java
@@ -31,4 +31,62 @@ public class LinkedList {
         }
         return de.next;
     }
+
+    //旋转链表
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null ||k<=0) return head;
+        ListNode p = head;
+        int i=1;
+        while(p.next!=null) {
+            p = p.next;
+            i++;
+        }
+        p.next = head;
+        p = head;
+        int count = i-k%i;
+        while(count>0) {
+            p = p.next;
+            count--;
+        }
+        head = p;
+        while(i>1) {
+            head = head.next;
+            i--;
+        }
+        head.next = null;
+        return p;
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head ==null) return null;
+        ListNode h = new ListNode(-1);
+        h.next = head;
+        ListNode p = head,prev = h;
+        while (p != null&&p.next!=null) {
+            ListNode q = p.next;
+            if(p.val!=q.val) {
+                 prev = p;
+                 p = p.next;
+            }else{
+                while(p.next!=null&&p.val == p.next.val) {
+                    p = p.next;
+                }
+                prev.next = p.next;
+                p = p.next;
+            }
+        }
+        return h.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode h1 = new ListNode(2);
+        ListNode h2 = new ListNode(2);
+        ListNode h3 = new ListNode(4);
+        ListNode h4 = new ListNode(5);
+        head.next = h1;h1.next = h2;h2.next = h3;
+        h3.next = h4;
+        new LinkedList().deleteDuplicates(head);
+        System.out.println();
+    }
 }
--- src/main/java/com/kevin/leecode/online/simple/ListNode.java
+++ src/main/java/com/kevin/leecode/online/simple/ListNode.java
@@ -4,8 +4,8 @@ package com.kevin.leecode.online.simple;
  * @author kevin lau
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
